91a48bb0c60b3c12779eb9f051dc88c73a14ab9f
Omkar <om.sathe@egmail.com>
2022-09-17 21:49:14 +0530

    Finished game function

--- javascript.js
+++ javascript.js
@@ -34,15 +34,19 @@ function playRound(playerSelect, compSelect) {
 }
 
 function game() {
-    
+    let random_number;
+    let min = 1;
+    let max = 3;
+    let playerSelect;
+    let compSelect;
+    for (let i = 0; i < 5; i++) {
+        random_number = Math.floor(Math.random() * (max - min + 1)) + min;
+        compSelect = (getComputerChoice(random_number));
+        playerSelect = prompt("What do you choose?");
+        console.log(playRound(playerSelect, compSelect));
+    }
 }
 
-let random_number;
-let min = 1;
-let max = 3;
-let playerSelect;
-let compSelect;
-random_number = Math.floor(Math.random() * (max - min + 1)) + min;
-compSelect = (getComputerChoice(random_number));
-playerSelect = prompt("What do you choose?");
-console.log(playRound(playerSelect, compSelect));
+game(); 
+
+
